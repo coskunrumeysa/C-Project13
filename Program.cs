5add9695fa66f6a726279f51fd1de250ece0f263
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-27 17:26:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara 78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porject16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kk;
+            int uk;
+            int area;
+            int perimeter;
+
+            Console.WriteLine("Please enter short side of rectangle :");
+            kk = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please enter long side of rectangle :");
+            uk = Convert.ToInt16(Console.ReadLine());
+
+            area = kk * uk;
+            perimeter = 2*kk+2*uk;
+
+            Console.WriteLine("Area of rectangle:" + " " + area);
+
+            Console.WriteLine("Perimeter of rectangle:" + " " + perimeter);
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
